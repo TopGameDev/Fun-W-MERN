b7a129f1de213339bbe4d7b1b5c144b2c216b541
GameDev <gamemaster@GME.local>
2023-09-29 16:32:19 -0500

    Added Post and User Routes w/ Auth

--- server/index.js
+++ server/index.js
@@ -1,3 +1,10 @@
+import { register } from "./controllers/auth.js";
+import { fileURLToPath } from "url";
+import { verifyToken } from "./middleware/auth.js";
+import { createPost } from "./controllers/posts.js"
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js"
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
@@ -7,9 +14,6 @@ import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
-import { fileURLToPath } from "url";
-import authRoutes from "./routes/auth.js"
-import { register } from "./controllers/auth.js"
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -39,9 +43,12 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 5555;
